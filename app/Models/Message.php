b465e37abd328a58d79b36ac3f3e641350a6b11f
Juang Salaz Prabowo <juang.salaz.prabowo@gmail.com>
2024-04-18 17:41:13 +0700

    first commit

---
+++ app/Models/Message.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Message extends Model
+{
+    use HasFactory;
+
+    /**
+     * Get the user that owns the Message
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function fromUser()
+    {
+        return $this->belongsTo(User::class, 'from_user_id');
+    }
+
+    /**
+     * Get the user that owns the Message
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function toUser()
+    {
+        return $this->belongsTo(User::class, 'to_user_id');
+    }
+}
